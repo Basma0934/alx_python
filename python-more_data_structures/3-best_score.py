14ac50bf462fda6a3d83a4efef8f3bafd0da84c2
Basma Bellaji <basma.bellaji@gmail.com>
2023-12-07 20:18:13 +0100

    best score commit

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,5 +1,14 @@
 def best_score(a_dictionary):
     """Returning a key with the biggest integer value."""
-    if len(a_dictionary) == 0:
+    if a_dictionary == {} or a_dictionary == None:
         return None
+    else:
+        BiggestValue = max(list(a_dictionary.values()))
+        for key in a_dictionary:
+            if a_dictionary[key] == BiggestValue:
+                thebest_score = key
+        return thebest_score
+    
+
+
 
