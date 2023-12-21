b848d26cb1bfce4b5930a38add1e998666fa39b3
Basma Bellaji <basma.bellaji@gmail.com>
2023-12-21 14:04:53 +0100

    Sqaure commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,5 +2,6 @@
 
 class Square():
     """Write a class that defines a square by private instance attribute: size."""
+    
     def __init__(self, size):
         self.__size = size
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,12 +1,12 @@
 """Define a class Square."""
 
-class Square:
-    """Represent a square."""
+class Square():
+    """Write a class that defines a square by private instance attribute: size."""
 
-    def __init__(self, size):
-        """Initialize a new Square.
-
-        Args:
-            size (int): The size of the new square.
-        """
+    def __init__(self, size=0):
+          
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
