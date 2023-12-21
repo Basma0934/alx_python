bfb832a4f25747d69afdd1fbeaad3336a79db09c
Basma Bellaji <basma.bellaji@gmail.com>
2023-12-21 15:14:02 +0100

    square commit

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -33,5 +33,6 @@ class Square():
         if self.__size == 0:
             print()
         for i in range(0, self.__size):
-            print("#", end="")
+            [print("#", end="") for j in range(self.__size)]
+            print("")
         
