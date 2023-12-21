a2e813d21fb2366b59b5eadc6e1f46a19dfdf651
Basma Bellaji <basma.bellaji@gmail.com>
2023-12-21 15:02:42 +0100

    square commit

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,20 +1,29 @@
 """Define a class Square."""
 
-class Square:
-    """Represent a square."""
+class Square():
+    """write a class that defines a square by private instance attribute: size."""
 
     def __init__(self, size=0):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
-        """
+    
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
+    
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
