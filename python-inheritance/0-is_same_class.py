86cc5fe42db54252d1d59bc445ae2c2094be32c9
Basma Bellaji <basma.bellaji@gmail.com>
2023-12-24 23:32:25 +0100

    0 is same class commit

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,12 +1,15 @@
 def is_same_class(obj, a_class):
-    """Check if an object is exactly an instance of a given class.
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
-    Returns:
-        If obj is exactly an instance of a_class - True.
-        Otherwise - False.
-    """
-    if type(obj) == a_class:
-        return True
-    return False
+    """Check if an object is exactly an instance of a given class."""
+
+    if a_class == object:
+#         return False
+#     elif type(obj) == bool:
+#         return False
+#     else:
+#         class_name = type(obj)
+#         if issubclass(class_name, a_class):
+#             return True
+#         else:
+#             return False
+        
+        return type(obj) is a_class
