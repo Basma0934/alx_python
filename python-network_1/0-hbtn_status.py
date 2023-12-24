1571e9b130dcb8cf18617dea879170d3c82de32b
Basma Bellaji <basma.bellaji@gmail.com>
2023-12-24 21:58:09 +0100

    hbtn status commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,8 @@
 """Writing a Python script that fetches https://alu-intranet.hbtn.io/status"""
-import urllib.request
+import requests
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+request = requests.get('https://alu-intranet.hbtn.io/status')
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode("utf-8")))
