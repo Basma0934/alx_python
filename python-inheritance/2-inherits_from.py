c220396446b54e3c28645af307f93300b6a41e9d
Basma Bellaji <basma.bellaji@gmail.com>
2023-12-24 23:47:57 +0100

    inherits from commit

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,6 +1,5 @@
-def inherits_from(obj, a_class):
-    """Checks if if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
-    
+"""Checks if if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
+
     Args:
         obj (any): The object to check.
         a_class (type): The class to match the type of obj to.
@@ -8,8 +7,8 @@ def inherits_from(obj, a_class):
         If obj is an inherited instance of a_class - True.
         Otherwise - False.
 
-    """
-
+"""
+def inherits_from(obj, a_class):
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
