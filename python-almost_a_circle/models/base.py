ba5d8373917779baaa1972e3f22bdc000af20694
Basma Bellaji <basma.bellaji@gmail.com>
2023-12-25 12:31:00 +0100

    base commit

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,22 +1,18 @@
-"""Base python file"""
+"""
+a python file for base
+"""
 
 class Base:
     """
-    Represents the Base model for all other classes on this project
-
-    Attributes:
-        __nb_objects (int): The number of instantiated Bases.
+    a Base class
     """
     __nb_objects = 0
-
     def __init__(self, id=None):
-         """Initialize a new Base.
-
-        Args:
-            id (int): The identity of the new Base.
         """
-    if id is not None:
-        self.id = id
-    else:
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+        init method
+        """
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects +=1
+            self.id = Base.__nb_objects
