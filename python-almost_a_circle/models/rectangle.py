874d955cbfe01a3502a5e39ddd9169b72e6e20d3
Basma Bellaji <basma.bellaji@gmail.com>
2023-12-25 13:22:40 +0100

    rectangle commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -36,6 +36,32 @@ class Rectangle(Base):
         
         if y < 0:
             raise ValueError("{} must be >= 0".format("y"))
+        
+    @property
+    def width(self):
+        """Set/get the width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Set/get the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
        
     
